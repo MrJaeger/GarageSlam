ec7ba6f5d403a59f71e05ccb38fc01e3c58cb51d
Andrew Jaeger <mrjaeger00@gmail.com>
2012-01-14 18:08:22 -0500

    api stuff

--- api/getUser.php
+++ api/getUser.php
@@ -4,11 +4,18 @@ $id = $_GET['id'];
 
 $mySQLConnection = mysqli_connect("mysql.slamwhale.com", "slamwhale", "cloudwhale00", "garageslam");
 
-$user = mysql_fetch_assoc(
-		mysql_query(
-			"select * from `users` where `tid` = '" . $id . "'";
+$user = mysqli_fetch_assoc(
+		mysqli_query(
+			$mySQLConnection, "select * from `users` where `id` = '" . $id . "'"
 		)
 );
+
+$user['influences'] = array();
+$iQuery = "SELECT * from influences WHERE id = $id";
+$iResult = mysqli_query($mySQLCOnnection, $iQuery);
+while($row = mysqli_fetch_assoc($iResult)) {
+	$user['influences'][] = $row['bandName'];
+}
 
 echo json_encode($user);
 
