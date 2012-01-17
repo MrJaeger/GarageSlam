0a3826a77959266aa06887b82bf37c36298a866c
Dan Mundy <drm.mundy@gmail.com>
2012-01-16 19:06:17 -0500

    Adds an about page and a footer.

--- footer.php
+++ footer.php
@@ -1,2 +1,7 @@
+		<div class="wrapper">
+			<div class="footer">
+				<a href="/about.php">About</a> &bull; <a href="http://www.twitter.com/garageslam">Twitter</a> &bull; &copy; 2012 <a href="http://twitter.com/exchgr">Dan Mundy</a> and Andrew Jaeger.
+			</div><!--/.footer-->
+		</div><!--/.wrapper-->
 	</body>
 </html>
