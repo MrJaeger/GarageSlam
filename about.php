b96d7e52ecfe053d970c9ee10198ee6fd2a100c7
Dan Mundy <drm.mundy@gmail.com>
2012-01-16 19:11:21 -0500

    Fixes #14: Adds an about page (for real this time) and a footer.

---
+++ about.php
@@ -0,0 +1,18 @@
+<? require_once($_SERVER['DOCUMENT_ROOT'] . "/header.php"); ?>
+		<div class="wrapper">
+			<h1>About GarageSlam</h1>
+			<p>GarageSlam is a social network for connecting local musicians. Born at the <a href="http://2012s.pennapps.com/">PennApps Spring 2012</a> hackathon, it was designed and built by Dan Mundy, an English major at The College of New Jersey, and Andrew Jaeger, a Computer Science major at Rutgers University. At PennApps, GarageSlam won the award for best design. Credit for the original idea goes to Dan and Andrew's friend, Graham Makely.</p>
+			<h2>Props</h2>
+			<p>GarageSlam uses a number of other sites to help make it awesome.</p>
+			<ul class="no-bullet">
+				<li><a href="http://facebook.com/"><img src="http://www.facebook.com/favicon.ico" class="icon" /> Facebook</a> helps you log in seamlessly.</li>
+				<li><a href="http://the.echonest.com/"><img src="http://the.echonest.com/favicon.ico" class="icon" /> The Echonest</a> provides photos for all your influences.</li>
+				<li><a href="http://www.soundcloud.com/"><img src="http://soundcloud.com/favicon.ico" class="icon" /> SoundCloud</a> shares the music you've made with everyone.</li>
+				<li><a href="http://maps.google.com/"><img src="http://maps.gstatic.com/favicon.ico" class="icon" /> Google Maps</a> helps search for people near a location.</li>
+			</ul>
+			<h2>GarageSlam around the Web</h2>
+			<ul>
+				<li><a href="http://www.twitter.com/garageslam">GarageSlam on Twitter</a></li>
+			</ul>
+		</div><!--/.wrapper-->
+<? require_once($_SERVER['DOCUMENT_ROOT'] . "/footer.php"); ?>
